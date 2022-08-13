05d3a03bc2e73854776ffc212829ac54e4d02dd4
himal999 <himal.official99@gmail.com>
2022-08-13 18:47:28 +0530

    express routes

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser"
-
+import userRoutes from "./routes/users.js"
 const app = express();
 const PORT  = 5000;
 
 app.use(bodyParser.json())
+
+app.use('/users',userRoutes )
+
+app.get('/',(req,res)=>{
+    console.log("test get")
+    res.send("from homepage")
+})
 
 app.listen(PORT,()=>{
     console.log("server Running")
