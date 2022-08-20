90137eabd50e541f4c8cd370fd978c3238be00d3
himal999 <himal.official99@gmail.com>
2022-08-20 09:52:19 +0530

    post request

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,24 @@
 import express from "express"
+import { v4 as uuidv4 } from "uuid";
+uuidv4();
 
 const router  = express.Router();
 
-const users = [
-    {
-        firstName:"himal",
-        lastName:"ayanth",
-        age:25
-    }
-]
+const users = []
 
 router.get('/',(req,res)=>{ 
-    console.log(users)
-    res.send("hi")
+   
+    res.send(users)
 });
+
+router.post('/',(req,res)=>{
+    const user = req.body;
+
+    const userId = uuidv4();
+
+    const idWithUser = ({...user,id:userId})
+    users.push(idWithUser)
+    res.send("user added !!!")
+})
 
 export default router;
